9deffc10817a81e374149c637a3c13d8ab64ef4c
gquadrati <greta.quadrati@pagopa.it>
2021-10-19 15:02:24 +0200

    minor changes

--- scripts/check_referenced_variables.sh
+++ scripts/check_referenced_variables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+attributes_file_path="https://raw.githubusercontent.com/pagopa/io-functions-template/add_function_json_check/scripts/attributes.json"
+attributes_to_check=($(curl ${attributes_file_path} | jq -r ".attributes[]"))
 
-attributes_to_check=$(jq ".attributes[]" "./attributes.json")
-
-
-errors=false
 functions_dir=($(find . -name function.json -print0 | xargs -0 -n1 dirname | sort --unique))
 
+errors=false
 for curr_function_dir in "${functions_dir[@]}"; do
     curr_function_json="${curr_function_dir}/function.json"
+
     for attribute in "${attributes_to_check[@]}"; do
         referenced_in_env=$(jq ".bindings[] | select( has(\"${attribute}\") == true) | .${attribute} | startswith(\"%\")" "${curr_function_json}")
         if [ "$referenced_in_env" = false ] 
